1b9eceb44cf679e1f6668aab9069588f7bf884db
Ruaneri Portela <ruaneriportela@outlook.com>
2024-01-15 11:24:02 -0300

    AutoBuild msbuild on SH

--- deployTest.sh
+++ deployTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -f ./x64/Release/*.exe /y/
-cp -f ./portaudioMSVC2022/x64/Release/*.dll /y/
+cp -f ./x64/Release/*.dll /y/
 paDLL=$(which libportaudio.dll)
 cp -f $paDLL /y/
 cp -f ./build/*.exe /y/ShareAudio_GCC.exe
--- packingWindows.sh
+++ packingWindows.sh
@@ -4,20 +4,26 @@ make
 # packing for MSVC
 version=$(git describe --tags)
 builds=("Win32" "ARM64" "x64")
-deps=(./portaudioMSVC2022)
 for build in "${builds[@]}"; do
-    mkdir -p ./build/$build 
-    for dep in "${deps[@]}"; do
-        cp $dep/$build/Release/*.dll ./build/$build/ -f
-    done
-    cp ./$build/Release/*.exe ./build/$build/ -f
-    zip "./build/ShareAudio_MSVC_$build($version).zip" ./build/$build/* -j
-    rm -rf ./build/$build
+    if [ "$build" == "Win32" ]; then
+        msbild="x86" 
+    else
+        msbild="$build"
+    fi
+    "/c/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe" ShareAudio.sln -t:Clean -p:Configuration=Release -p:Platform="$msbild"
+    "/c/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe" ShareAudio.sln -p:Configuration=Release -p:Platform="$msbild"
+    "/c/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe" ShareAudio.sln -t:Clean -p:Configuration=Debug -p:Platform="$msbild"
+    "/c/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe" ShareAudio.sln -p:Configuration=Debug -p:Platform="$msbild"
+    mkdir -p "./build/$build" 
+    cp "./$build/Release/"*.exe "./build/$build/" -f
+    cp "./$build/Release/"*.dll "./build/$build/" -f
+    zip "./build/ShareAudio_MSVC_$build($version).zip" "./build/$build/"* -j
+    rm -rf "./build/$build"
 done
 # packing for x64_GNU_GCC
-mkdir -p ./build/x64_GNU_GCC
+mkdir -p "./build/x64_GNU_GCC"
 paDLL=$(which libportaudio.dll)
-cp $paDLL ./build/x64_GNU_GCC/ -f
-cp ./build/*.exe ./build/x64_GNU_GCC -f
-zip "./build/ShareAudio_GNU-GCC_x64($version).zip" ./build/x64_GNU_GCC/* -j
-rm -rf ./build/x64gcc
+cp "$paDLL" "./build/x64_GNU_GCC/" -f
+cp "./build/"*.exe "./build/x64_GNU_GCC" -f
+zip "./build/ShareAudio_GNU-GCC_x64($version).zip" "./build/x64_GNU_GCC/"* -j
+rm -rf "./build/x64_GNU_GCC"
