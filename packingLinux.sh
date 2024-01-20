bc58f017d3546f3d76d2bdf580565d227ae93a64
Ruaneri Portela <ruaneriportela@outlook.com>
2024-01-20 00:42:15 -0300

    Linux Sh build packing

---
+++ packingLinux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+GIT_VERSION=$(git describe --tags)
+rm -f .\src\VERSION.h
+echo "#define VERSION \"$GIT_VERSION\""
+make clean
+make
+zip -j "build/ShareAudio_GNU-GCC_x64($GIT_VERSION)_linux.zip" build/ShareAudio
