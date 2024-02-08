56afd8472c186ce8bd23f5ede87fe6dc04bfc63f
Ruaneri Portela <ruaneriportela@outlook.com>
2024-02-08 08:19:38 -0300

    Detect arch on build linux

--- packingLinux.sh
+++ packingLinux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 GIT_VERSION=$(git describe --tags)
+arch=$(arch)
 rm -f ./src/VERSION.h
 echo "#define VERSION \"$GIT_VERSION\"" >> ./src/VERSION.h
 make clean
 make
-zip -j "build/ShareAudio_GNU-GCC_x64($GIT_VERSION)_linux.zip" build/ShareAudio build/libShareAudio.so
+zip -j "build/ShareAudio_GNU-GCC_$arch($GIT_VERSION)_linux.zip" build/ShareAudio build/libShareAudio.so
