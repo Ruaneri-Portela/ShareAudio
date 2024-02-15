8f6c034d5fb4ecd12ef68c9a3063ca36dad2fa62
Ruaneri Ferreira Portela <91219330+Ruaneri-Portela@users.noreply.github.com>
2024-02-15 16:14:18 -0300

    Update

--- buildOpenSSLMsvc.bat
+++ buildOpenSSLMsvc.bat
@@ -1,8 +1,12 @@
-cd D:\Data\ShareAudio\openssl
+cd ShareAudio\openssl
+mkdir ..\msvc
+mkdir ..\msvc\openssl
+mkdir ..\msvc\openssl\builds
 set PATH=D:\Data\ShareAudio\msvc\openssl\nasm;D:\Data\ShareAudio\msvc\openssl\perl\perl\bin;%PATH%
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 nmake clean
 perl Configure VC-WIN64A && nmake && nmake test
+mkdir ..\msvc\openssl\builds\x64\
 copy .\*.dll ..\msvc\openssl\builds\x64\
 copy .\*.lib ..\msvc\openssl\builds\x64\
 copy .\*.exp ..\msvc\openssl\builds\x64\
@@ -11,6 +15,7 @@ copy .\*.pdb ..\msvc\openssl\builds\x64\
 nmake clean
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
 perl Configure VC-WIN32 && nmake && nmake test
+mkdir ..\msvc\openssl\builds\Win32\
 copy .\*.dll ..\msvc\openssl\builds\Win32\
 copy .\*.lib ..\msvc\openssl\builds\Win32\
 copy .\*.exp ..\msvc\openssl\builds\Win32\
@@ -19,6 +24,7 @@ copy .\*.pdb ..\msvc\openssl\builds\Win32\
 nmake clean
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64_arm64.bat"
 perl Configure VC-WIN64-ARM && nmake && nmake test
+mkdir ..\msvc\openssl\builds\ARM64\
 copy .\*.dll ..\msvc\openssl\builds\ARM64\
 copy .\*.lib ..\msvc\openssl\builds\ARM64\
 copy .\*.exp ..\msvc\openssl\builds\ARM64\
--- src/resource.h
+++ src/resource.h
@@ -1,11 +1,5 @@
-//{{NO_DEPENDENCIES}}
-// Arquivo de inclusão gerado pelo Microsoft Visual C++.
-// Usado por Resource.rc
-//
 #define IDI_ICON1                       103
 
-// Next default values for new objects
-// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
